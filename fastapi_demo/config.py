d8fefaf4fd8411a4fbd89e59e5c6b3cc83c57573
clark <dmc950213@gmail.com>
2025-10-16 21:29:49 +0800

    優化: 使用Depend統一處理Token邏輯

--- fastapi_demo/config.py
+++ fastapi_demo/config.py
@@ -2,19 +2,6 @@ from fastapi_demo.utility import get_env_value
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
-# # Store all env properties
-# SECRET_KEY = get_env_value("SECRET_KEY")
-# ALGORITHM = get_env_value("ALGORITHM")
-# ACCESS_TOKEN_EXPIRE_MINUTES = get_env_value("ACCESS_TOKEN_EXPIRE_MINUTES")
-# MINIO_ENDPOINT = get_env_value("MINIO_ENDPOINT")
-# MINIO_ACCESS_KEY = get_env_value("MINIO_ACCESS_KEY")
-# MINIO_SECRET_KEY = get_env_value("MINIO_SECRET_KEY")
-# BUCKET_NAME = get_env_value("BUCKET_NAME")
-# MONGO_URI = get_env_value("MONGO_URI")
-# DB_NAME = get_env_value("DB_NAME")
-# DATABASE_URL = get_env_value("DATABASE_URL")
-
-
 class Settings(BaseSettings):
     # .env 檔案路徑設定
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
--- fastapi_demo/routers/users.py
+++ fastapi_demo/routers/users.py
@@ -3,47 +3,52 @@ import uuid
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from .. import models, schemas
-from ..auth import verify_token
+from starlette import status
+from .. import models, schemas, auth
 from ..postgre import get_db, pwd_context
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@router.post("/", response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    username = verify_token(token)
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    username = auth.verify_token(token)
     if username is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    db_user = models.User(name=user.name, age=user.age, username=user.username,password=pwd_context.hash(user.password))
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # You could even fetch the user object from DB here if needed
+    return username  # or user object
+
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, current_user: str = Depends(get_current_user), db: Session = Depends(get_db)):
+    db_user = models.User(name=user.name, age=user.age, username=user.username,
+                          password=pwd_context.hash(user.password))
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
 @router.get("/", response_model=list[schemas.UserQueryResponse])
-def list_users(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    username = verify_token(token)
-    if username is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
+def list_users(current_user: str = Depends(get_current_user), db: Session = Depends(get_db)):
     return db.query(models.User).all()
 
+
 @router.get("/{user_id}", response_model=schemas.UserQueryResponse)
-def get_user(user_id: uuid.UUID, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    username = verify_token(token)
-    if username is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
+def get_user(user_id: uuid.UUID, current_user: str = Depends(get_current_user), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.qry_id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
+
 @router.put("/{user_id}", response_model=schemas.UserResponse)
-def update_user(user_id: uuid.UUID, updated_user: schemas.UserUpdate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    username = verify_token(token)
-    if username is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
+def update_user(user_id: uuid.UUID, updated_user: schemas.UserUpdate, current_user: str = Depends(get_current_user),
+                db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.qry_id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -54,10 +59,7 @@ def update_user(user_id: uuid.UUID, updated_user: schemas.UserUpdate, token: str
     return user
 
 @router.delete("/{user_id}")
-def delete_user(user_id: uuid.UUID, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    username = verify_token(token)
-    if username is None:
-        raise HTTPException(status_code=401, detail="Invalid token")
+def delete_user(user_id: uuid.UUID, current_user: str = Depends(get_current_user), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.qry_id == user_id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
